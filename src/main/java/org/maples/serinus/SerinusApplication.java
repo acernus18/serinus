3eafe10d19211142593d9b325cc47ce94fc1b598
acernus18 <acernus0108@gmail.com>
2019-04-22 13:16:42 +0800

    [Featurn]: init serinus web project.

---
+++ src/main/java/org/maples/serinus/SerinusApplication.java
@@ -0,0 +1,13 @@
+package org.maples.serinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerinusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerinusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/maples/serinus/SerinusApplicationTests.java
@@ -0,0 +1,16 @@
+package org.maples.serinus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerinusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
